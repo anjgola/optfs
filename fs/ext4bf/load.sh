d6e3cfdeef68dccf683ce4f0194dbf46d0c75561
root <root@ubuntu.(none)>
2013-11-08 17:05:39 -0600

    debug commit

--- fs/ext4bf/load.sh
+++ fs/ext4bf/load.sh
@@ -3,9 +3,10 @@ modprobe -r ext4bf
 #cd ..
 make
 make modules_install
+cp ./ext4bf.ko /lib/modules/3.2.0vijaycbf-g0c7464b-dirty/
 echo "Loading module"
 modprobe ext4bf
 echo "Mounting fs"
-mount -t ext4bf -o nodelalloc,nobarrier,nouser_xattr,noacl,data=journal  /dev/sdb1 /mnt/mydisk
+mount -t ext4bf -o nodelalloc,nobarrier,nouser_xattr,noacl,data=journal  /dev/sda /mnt/mydisk
 echo "Fs contents"
 ls /mnt/mydisk/
--- fs/ext4bf/mk-big.sh
+++ fs/ext4bf/mk-big.sh
@@ -1,2 +1,2 @@
 umount /mnt/mydisk
-mkfs.ext4 -j -J size=16384 -E lazy_itable_init=0,lazy_journal_init=0 /dev/sdb1
+mkfs.ext4 -j -J size=16384 -E lazy_itable_init=0,lazy_journal_init=0 /dev/sda
